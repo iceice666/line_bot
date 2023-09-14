1998aada1a59a094dd711783081778fb3e592d1f
iceice666 <iceice666@outlook.com>
2023-09-15 01:36:20 +0800

    test which provider support content convsersation

--- src/main.py
+++ src/main.py
@@ -1,14 +1,41 @@
 import g4f
+import pprint
 
+prompt: list[dict[str, str]] = []
+
+AVAILBLE_PROVIDERS = [
+    # model="gpt-3.5-turbo",
+    g4f.Provider.ChatgptAi,
+    g4f.Provider.DeepAi,
+
+    # model="gpt-4",
+    g4f.Provider.Aivvm,
+]
 
 while True:
-    ask = input("Ask AI >")
+    ask = input("Ask AI > ")
+
+    prompt.append({"role": "user", "content": ask})
 
     response = g4f.ChatCompletion.create(
-        messages=[{"role": "user", "content": ask}],
+        messages=prompt,
         model="gpt-4",
-        provider=g4f.Provider.Bing,
-        stream=True,
+        provider=g4f.Provider.Aivvm,
+        stream=False,
     )
 
-    print(response)
+    ans = ""
+
+    for i in response:
+        if i == "":
+            ans += "\n"
+        else:
+            ans += i
+
+    prompt.append({"role": "assistant", "content": ans})
+
+    pprint.pprint(prompt)
+
+    print()
+
+    print(ans)
